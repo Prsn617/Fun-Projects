329fa30003dc8074c83623f45049efb8305eff38
Prsn <prsn617@gmail.com>
2024-05-21 22:51:45 +0545

    Added colors and download btn for Drawing App

--- Drawing App/canvas.js
+++ Drawing App/canvas.js
@@ -5,19 +5,26 @@ canvas.height = window.innerHeight;
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
 
+const buttons = document.querySelectorAll(".btn");
+const clearScreen = document.querySelector(".cls");
+const downloadLnk = document.getElementById("downloadLnk");
+
 let c = canvas.getContext("2d");
-let background = "#f0f0f0";
-c.fillStyle = background;
+c.fillStyle = "#fff";
 c.fillRect(0, 0, innerWidth, innerHeight);
 
-function drawCircle(x, y, r, color) {
+function drawCircle(x, y, r, clr) {
   c.beginPath();
   c.arc(x, y, r, 0, 2 * Math.PI);
-  c.fillStyle = color;
+  c.fillStyle = clr;
   c.fill();
 }
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function download() {
+  let dt = canvas.toDataURL("image/png");
+  this.href = dt;
 }
 
 let color = "black";
@@ -27,8 +34,8 @@ class Circle {
   constructor(x, y, radius, color) {
     this.x = x;
     this.y = y;
-    this.color = color;
     this.radius = radius;
+    this.color = color;
   }
   draw() {
     drawCircle(this.x, this.y, this.radius, this.color);
@@ -39,23 +46,37 @@ class Circle {
   }
 }
 
+clearScreen.addEventListener("click", () => {
+  c.clearRect(0, 0, WIDTH, HEIGHT);
+  c.beginPath();
+  console.log("Bruh");
+});
+
+downloadLnk.addEventListener("click", download, false);
+
 let circleList = [];
-function init() {
-  circleList = [];
-}
 
 function animate() {
+  clearScreen.addEventListener("click", () => {
+    c.clearRect(0, 0, WIDTH, HEIGHT);
+    circleList = [[]];
+  });
   requestAnimationFrame(animate);
-  c.fillStyle = background;
-  c.fillRect(0, 0, innerWidth, innerHeight);
+  c.clearRect(0, 0, WIDTH, HEIGHT);
 
   width = document.getElementById("brushSlider").value;
+  for (let button of buttons) {
+    button.addEventListener("click", (e) => {
+      color = e.target.id;
+    });
+  }
 
   for (let i = 0; i < circleList.length; i++) {
     for (let j = 0; j < circleList[i].length; j++) {
-      circleList[i][j].draw(width);
+      circleList[i][j].draw();
       if (j < circleList[i].length - 1) {
-        c.lineWidth = 1;
+        c.lineWidth = parseInt(circleList[i][j].radius * 2);
+        c.strokeStyle = circleList[i][j].color;
         c.beginPath();
         c.moveTo(circleList[i][j].x, circleList[i][j].y);
         c.lineTo(circleList[i][j + 1].x, circleList[i][j + 1].y);
@@ -65,7 +86,6 @@ function animate() {
   }
 }
 
-init();
 animate();
 
 let isDragging = false;
@@ -82,7 +102,7 @@ addEventListener("mousemove", (e) => {
       circleList.push([]);
     }
     circleList[circleList.length - 1].push(
-      new Circle(e.clientX, e.clientY, width, color)
+      new Circle(e.clientX, e.clientY, width / 2, color)
     );
   }
 });
